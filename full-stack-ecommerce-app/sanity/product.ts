7a2325da37e076b8f296db7beb93111960ea5c4f
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-08-25 23:53:58 +0500

    sanity completely updated

--- full-stack-ecommerce-app/sanity/product.ts
+++ full-stack-ecommerce-app/sanity/product.ts
@@ -1,45 +1,54 @@
-export const product = {
+import { defineField, defineType } from "sanity";
+
+
+export default defineType({
+
+
     name: 'product',
     type: 'document',
     title: 'Product',
     fields: [
-        {
+        defineField( {
             name:'title',
             title: 'Title',
             type: 'string'
 
-        },
-        {
+        }),
+        defineField( {
             name:'price',
             title: 'Prodect Price',
             type: 'number'
 
-        },
-        {
+        }),
+        defineField({
             name:'discription',
             title: 'Discription',
             type: 'string'
 
-        },
-        {
+        }),
+        defineField({
             name:'image',
             title: 'Prodect image',
             type: 'image'
 
-        },
-        {
+        }),
+        defineField({
             name:'category',
             title:'Prodect Category',
             type:'string',
-            list:[
-                
+            options:{
+
+
+                list:[
+                    
                     { title: "Mens", value: "mens" },
                     { title: "Womens", value: "womens" },
                     { title: "Kids", value: "kids" },
+                ]
+            }
                     
                 
-            ]
-        }
+        })
     ]
 
-}
+})
