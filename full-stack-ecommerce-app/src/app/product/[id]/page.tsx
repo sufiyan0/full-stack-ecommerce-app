ac1106d3ea009d3a7e84e451334a6c90c2471579
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-08-28 14:44:01 +0500

    updatae

--- full-stack-ecommerce-app/src/app/product/[id]/page.tsx
+++ full-stack-ecommerce-app/src/app/product/[id]/page.tsx
@@ -1,7 +1,10 @@
 import { client } from "../../../../sanity/lib/client";
 import { getClient } from "../../data";
 import { Image as IImage } from "sanity";
-import {fulldata} from '../../data'
+import { fulldata } from "../../data";
+import Image from "next/image";
+import { urlForImage } from "../../../../sanity/lib/image";
+import { resourceUsage } from "process";
 
 interface IProduct {
   price: string;
@@ -12,15 +15,93 @@ interface IProduct {
 }
 
 export default async function Page({ params }: { params: { id: string } }) {
-const getProducctDetail = async () => {
-  const products: IProduct[] = await getClient();
-    // console.log(products)
-  const newdata = products.find((product) => product._id == params.id);
-  // console.log(newdata)
-  return newdata;
-};
-  const result = await getProducctDetail();
-  console.log(result);
+  const getProducctDetail = async () => {
+    const products: IProduct[] = await getClient();
+    const newdata = products.find((product) => product._id == params.id);
+    return newdata;
+  };
 
-  return <div className="mt-36">My Post: {params.id}</div>;
+  const result:any = await getProducctDetail();
+  // console.log(result);
+
+  return (
+    <>
+      <section className="text-gray-600 body-font overflow-hidden min-h-screen mt-32">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <Image
+              alt="ecommerce"
+              className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
+              src={urlForImage(result.image).url()}
+              width={400}
+              height={200}
+            />
+            <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
+              <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                {result.title}
+              </h2>
+              <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
+              {result.title}
+              </h1>
+             <p className="leading-relaxed">
+                {result.discription}
+              </p>
+              <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
+                {/* <div className="flex">
+                  <span className="mr-3">Color</span>
+                  <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
+                  <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button>
+                </div> */}
+                <div className="flex ml-6 items-center">
+                  <span className="mr-3">Size</span>
+                  <div className="relative">
+                    <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base pl-3 pr-10">
+                      <option>SM</option>
+                      <option>M</option>
+                      <option>L</option>
+                      <option>XL</option>
+                    </select>
+                    <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                      <svg
+                        fill="none"
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        className="w-4 h-4"
+                        viewBox="0 0 24 24"
+                      >
+                        <path d="M6 9l6 6 6-6"></path>
+                      </svg>
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="flex">
+                <span className="title-font font-medium text-2xl text-gray-900">
+                 $  {result.price}
+                </span>
+                <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+                  add to cart
+                </button>
+                {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
+                  <svg
+                    fill="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    className="w-5 h-5"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
+                  </svg>
+                </button> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
