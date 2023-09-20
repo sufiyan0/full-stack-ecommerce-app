a59835a9cd5c34d5dae6c187a1f28e67f5df685d
Sufiyan_Ahmed <sufiyanlaiq4@gmail.com>
2023-09-20 23:35:19 +0500

    add serversideprops

--- ecommerce/src/app/page.tsx
+++ ecommerce/src/app/page.tsx
@@ -1,19 +1,43 @@
-import React from "react"
-import {Product, FooterBanner,HeroBanner,Footer} from './components'
-export default function Home() {
+import React from "react";
+import { Product, FooterBanner, HeroBanner, Footer } from "./components";
+import { client } from "../lib/sanityClient";
+
+const getClient = async () => {
+  const res = await client.fetch(`*[_type=='product' ]`)
+
+  return res
+}
+
+export default async function Home({ products, bannerData }:any) {
+  const data:any = await getClient()
+
   return (
+
     <>
-      <HeroBanner/> 
+      <HeroBanner />
       <div className="products-heading">
         <h2>Best Selling Products</h2>
         <p>Speakers of many variation</p>
-      </div>   
-
+      </div>
+      
       <div className="products-container">
+      
         <p>products</p>
       </div>
 
-      <FooterBanner/>
+      <FooterBanner />
     </>
-  )
+  );
 }
+
+export const getServerSideProps = async () => {
+  const query = `*[_type == "product"]`;
+  const products = await client.fetch(query);    
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { products, bannerData },
+  };
+};
--- ecommerce/src/lib/sanityClient.ts
+++ ecommerce/src/lib/sanityClient.ts
@@ -2,7 +2,7 @@ import { createClient } from "next-sanity";
 
 
 export const client = createClient({
-    apiVersion:'6-9-2023',
+    apiVersion:process.env.NEXT_PUBLIC_SANITY_VERSION,
     projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset:process.env.NEXT_PUBLIC_SANITY_DATASET,
     useCdn:false,
